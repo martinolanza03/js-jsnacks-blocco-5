d586277119854baf28e18c1806537cc217111c85
martinolanza03 <lanzamartino3@gmail.com>
2025-03-12 13:59:26 +0100

    feat: snack 8

---
+++ snack-8/script.js
@@ -0,0 +1,33 @@
+const students = [
+  {
+    id: 1,
+    name: 'Marco Lanci',
+    age: 32,
+    class: '3C'
+  },
+  {
+    id: 2,
+    name: 'Mario Banfi',
+    age: 34,
+    class: '4A'
+  },
+  {
+    id: 3,
+    name: 'Luigi Banzi',
+    age: 33,
+    class: '5B'
+  },
+];
+
+// Recupera la classe dello studente 'Marco Lanci'
+// Risultato: '3C'
+
+const studentIdTwo = students.find((element) => {
+
+  if (element.name === `Marco Lanci`) {
+    return element.class;
+  }
+
+});
+
+console.log(studentIdTwo.class);
