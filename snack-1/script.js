f1d54039a6108e1a81f6206b611e7d01dd488443
martinolanza03 <lanzamartino3@gmail.com>
2025-03-12 13:31:31 +0100

    feat: first snacks

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach(element => {
+    console.log(element);
+});
